f9e4786275870d58ca1a86d9cb7521395015c77d
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-05-22 12:43:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
+DATABASE_URI = environ.get('DATABASE_URI', "capal90951")
+DATABASE_NAME = environ.get('DATABASE_NAME', "mongodb+srv://capal90951:Ea5ixpHnXwd1dvop@cluster0.vimg7oq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
